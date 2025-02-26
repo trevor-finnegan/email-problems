95b3bf30e9193cf3ecccd65a76cc15f792fe7af0
trevor-finnegan <finntd>
2025-02-26 17:23:45 -0500

    removed old, commented out code

--- src/components/Login.js
+++ src/components/Login.js
@@ -13,19 +13,6 @@ const Login = ({ setIsAuthenticated }) => {
     <div className="login-page">
       <div className="login-box">
         <h2>LOGIN</h2>
-        {/*errorMessage && <p className="error-message">{errorMessage}</p>*/}
-{/*}
-        <div className="google-login-container">
-          <GoogleLogin
-            onSuccess={handleGoogleLoginSuccess}
-            onError={() => console.log('Login Failed')}
-            useOneTap={false}  // Optional: Disables the one-tap sign-in feature
-            theme="outline"    // Button style for a traditional look
-            scope="https://www.googleapis.com/auth/gmail.readonly"
-            text={"Choose gmail account"}
-          />
-        </div>
-        */}
         <button onClick={handleAuthClick}>Sign in with Google</button>
       </div>
     </div>
