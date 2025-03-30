1cf35d1022363af67b19079bb88a8805cf57ab59
marioauqui <auqumo21@wfu.edu>
2025-03-30 15:58:42 -0400

    Email folder update: When a user adds an email to a folder, update the email's folder_id in the database. Updated API and DB setup accordingly.

--- email-api/server.js
+++ email-api/server.js
@@ -13,5 +13,6 @@ app.use("/folders", require("./routes/folders"));
 app.use("/emails", require("./routes/emails"));
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
