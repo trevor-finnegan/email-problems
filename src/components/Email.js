259747c41fe8070325cf602df5e662985f1ad939
lbj1533 <lxbxjx15@outlook.com>
2025-04-16 18:05:03 -0400

    put blue dot back

--- src/components/Email.js
+++ src/components/Email.js
@@ -9,16 +9,16 @@ const Email = ({ email, onSelectEmail, isSelected }) => {
     email.payload.headers.find((h) => h.name === "From")?.value ||
     "Unknown Sender";
 
-    return (
-      <div
-        onClick={() => onSelectEmail(email)}
-        className={`email-item ${isSelected ? "email-selected" : ""}`}
-      >
-        {email.isNew && <div className="email-new-dot" />}
-        <div className="email-subject">{subject}</div>
-        <div className="email-sender">{from}</div>
-      </div>
-    );
-  };
+  return (
+    <div
+      onClick={() => onSelectEmail(email)}
+      className={`email-item ${isSelected ? "email-selected" : ""}`}
+    >
+      {email.isNew && <div className="email-new-dot" />}
+      <div className="email-subject">{subject}</div>
+      <div className="email-sender">{from}</div>
+    </div>
+  );
+};
 
 export default Email;
